a199915bd8a3e675bdece0c9ca8cf467c0ffcc62
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2016-04-19 19:40:58 +0200

    adding hot reload, server.js, favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: 'eval',
   entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
     './src'
   ],
   output: {
@@ -12,12 +14,14 @@ module.exports = {
     publicPath: '/dist/'
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src'),
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
     }]
   }
 };
