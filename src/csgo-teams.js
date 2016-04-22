d9f6eb52da5504bb7fdc864605129b2098a5cf06
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2016-04-22 23:46:47 +0200

    add players inside team list

--- src/csgo-teams.js
+++ src/csgo-teams.js
@@ -2,52 +2,120 @@ export default [
   {
     name: "Natus Vincere",
     image: "http://static.hltv.org/images/teamLogos/4608.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4608"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4608",
+    players: [
+      "Edward",
+      "Zeus",
+      "GuardiaN",
+      "saized",
+      "flamie"
+    ]
   },
   {
     name: "Luminosity",
     image: "http://static.hltv.org/images/teamLogos/6290.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=6290"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=6290",
+    "players": [
+      "fnx",
+      "FalleN",
+      "fer",
+      "coldzera",
+      "TACO"
+    ]
   },
   {
     name: "fnatic",
     image: "http://static.hltv.org/images/teamLogos/4991.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4991"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4991",
+    "players": [
+      "olofmeister",
+      "dennis",
+      "flusha",
+      "JW",
+      "KRIMZ"
+    ]
 
   },
   {
     name: "Astralis",
     image: "http://static.hltv.org/images/teamLogos/6665.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=6665"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=6665",
+    "players": [
+      "karrigan",
+      "cajunb",
+      "Xyp9x",
+      "dupreeh",
+      "device"
+    ]
   },
   {
     name: "NiP",
     image: "http://static.hltv.org/images/teamLogos/4411.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4411"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4411",
+    "f0rest",
+    "GeT_RiGhT",
+    "Xizt",
+    "friberg",
+    "pyth"
   },
   {
     name: "EnVyUs",
     image: "http://static.hltv.org/images/teamLogos/5991.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5991"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5991",
+    "players": [
+      "kennyS",
+      "NBK-",
+      "apEX",
+      "Happy",
+      "DEVIL"
+    ]
   },
   {
     name: "Virtus.pro",
     image: "http://static.hltv.org/images/teamLogos/5378.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5378"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5378",
+    "players": [
+      "TaZ",
+      "NEO",
+      "pashaBiceps",
+      "Snax",
+      "byali"
+    ]
   },
   {
     name: "dignitas",
     image: "http://static.hltv.org/images/teamLogos/5422.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5422"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5422",
+    "players": [
+      "RUBINO",
+      "tenzki"
+      "MSL",
+      "Kjaerbye",
+      "k0nfig"
+    ]
   },
   {
     name: "mausesports",
     image: "http://static.hltv.org/images/teamLogos/4494.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4494"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=4494",
+    "players": [
+      "chrisJ",
+      "NiKo",
+      "nex",
+      "Spiidi",
+      "denis"
+    ]
   },
   {
     name: "Liquid",
     image: "http://static.hltv.org/images/teamLogos/5973.svg",
-    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5973"
+    hltvProfile: "http://www.hltv.org/?pageid=179&teamid=5973",
+    "players": [
+      "Hiko",
+      "nitr0",
+      "s1mple",
+      "EliGE",
+      "koosta"
+    ]
   }
 ]
